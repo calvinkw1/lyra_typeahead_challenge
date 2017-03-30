d312b4772e6641a6212e93b0358c800b2ac36749
Calvin Wang <calvinkw1@gmail.com>
2017-03-30 12:48:36 -0700

    typeahead functions working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,14 +9,58 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Lyra Typeahead Challenge</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="input-field">
+          <p>
+            <input type="text" onKeyUp={ getBooks } autoFocus />
+          </p>
+          <div id="results"></div>
+        </div>
       </div>
     );
   }
+}
+
+function getBooks() {
+  let input = document.getElementsByTagName("input")[0].value;
+  let booksArray = [];
+  makeRequest(input).then((result) => {
+    for (let i = 0; i < result.length; i++) {
+      let book = {
+        title: result[i].volumeInfo.title,
+        authors: result[i].volumeInfo.authors
+      }
+      booksArray.push(book);
+    }
+    createBookElement(booksArray);
+  });
+  
+}
+
+function makeRequest(input) {
+  const url = "https://www.googleapis.com/books/v1/volumes?q=" + input + "&maxResults=5"
+  return fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return data.items;
+    })
+    .catch(function(ex) {
+    console.log("Error", ex)
+  })
+}
+
+function createBookElement(books) {
+  console.log(books);
+  let booksList = books.map((book) =>
+      <ul>{book.title}, {book.authors} </ul>
+    )
+  ReactDOM.render(
+    <div>{booksList}</div>,
+    document.getElementById('results')
+  );
 }
 
 export default App;
