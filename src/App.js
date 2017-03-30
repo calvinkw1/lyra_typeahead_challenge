3720144fe490ceb08876055ba86816197ab16024
Calvin Wang <calvinkw1@gmail.com>
2017-03-30 16:26:06 -0700

    minor change to book info element

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function createBookElement(books) {
   let booksList = books.map((book, index) =>
       <div className="book-info" key={'book' + index}>
         <p>Title: {book.title}</p>
-        <p>Authors: {book.authors[0]}, {book.authors[1]} </p>
+        <p>Authors: {book.authors[0]} <br></br> {book.authors[1]} </p>
       </div>
     )
   ReactDOM.render(
